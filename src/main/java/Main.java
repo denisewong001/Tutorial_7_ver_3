176c93b04a52e702d81929f36ba9b312ad4c3d7d
denisewong001 <cww20@ic.ac.uk>
2022-11-22 17:25:44 +0000

    second commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,11 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! ");
+        printing();
+    }
+    public static void printing(){
+        for (int i = 1; i <= 10; i++){
+            System.out.println(i);
+        }
+    }
 }
